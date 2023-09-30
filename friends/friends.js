14f501be21296ec9c731f3e353255d11f856b8a9
Sơn <88760761+ahihison@users.noreply.github.com>
2023-09-30 14:30:31 +0700

    search edit profile tag filter friend

--- friends/friends.js
+++ friends/friends.js
@@ -1,25 +1,41 @@
-const triggerElement = document.getElementById("customTriggerElement");
-const tooltipContainer = document.getElementById("customTooltip");
-
-triggerElement.addEventListener("click", function () {
-  const rect = triggerElement.getBoundingClientRect();
-  const tooltipContent = triggerElement.getAttribute("title");
-
-  // Set tooltip content
-  tooltipContainer.textContent = tooltipContent;
-
-  // Position and show the tooltip always on the right
-  tooltipContainer.style.top = rect.top + window.scrollY + "px";
-  tooltipContainer.style.left = rect.right + window.scrollX + "px";
-  tooltipContainer.style.display = "block";
-
-  // Remove the title attribute to prevent native tooltip from appearing
-  triggerElement.removeAttribute("title");
-});
-
 document.addEventListener("click", function (event) {
   // Hide the tooltip when clicking outside the trigger element
   if (event.target !== triggerElement) {
     tooltipContainer.style.display = "none";
   }
 });
+function makeEditable(fieldName) {
+  const element = document.querySelector(`.${fieldName}`);
+  const currentValue = element.innerText;
+
+  // Create an input element
+  const inputElement = document.createElement("input");
+  inputElement.value = currentValue;
+
+  // Set the input element's class and focus
+  inputElement.className = "form-control";
+  inputElement.addEventListener("blur", () =>
+    updateValue(fieldName, inputElement.value)
+  );
+  inputElement.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      inputElement.blur();
+    }
+  });
+
+  // Replace the span with the input
+  element.replaceWith(inputElement);
+
+  // Focus the input
+  inputElement.focus();
+}
+
+function updateValue(fieldName, newValue) {
+  // Create a new span element
+  const spanElement = document.createElement("span");
+  spanElement.className = `editable ${fieldName}`;
+  spanElement.innerText = newValue;
+
+  // Replace the input with the span
+  document.querySelector(`.${fieldName}`).replaceWith(spanElement);
+}
